e33c9adf41d5b1e24455a2c44d2a986f28403f47
shaniba17 <shanibatm2002@gmail.com>
2024-06-13 15:18:44 +0530

    signUp completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const {blogmodel}=require("./models/blog")
 const bcrypt=require("bcryptjs") //importing bcryptjs for encryption
+const jwt=require("jsonwebtoken")
 
 mongoose.connect("mongodb+srv://shanibatm17:shaniba17tm@cluster0.h4a3e.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
 
@@ -25,6 +26,36 @@ app.post("/signup",async(req,res)=>
     blog.save()
     res.json({"status":"success"})
 })
+
+app.post("/signIn",(req,res)=>{
+    let input=req.body
+    blogmodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if (response.length>0) {
+                let dbPassword=response[0].password
+                console.log(dbPassword)
+                bcrypt.compare(input.password,dbPassword,(error,isMatch)=>
+                {
+                    if (isMatch) {
+                       jwt.sign({email:input.email},"blog-app",{expiresIn:"2h"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to found"})
+                            } else {
+                                res.json({"status":"success","userId":response[0]._id,"token":token})
+                            }
+                    })
+                    } else {
+                        res.json({"status":"incorrect"})
+                    }
+                })
+            } else {
+                res.json({"status":"user not found"})
+            }
+        }
+    ).catch()
+})
+
 
 app.listen(8080,()=>
 {
